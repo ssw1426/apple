51722c9dde90ed2b3fa15aa7869e02a1864423aa
ssw1426 <80308398+ssw1426@users.noreply.github.com>
2021-03-31 20:39:53 +0900

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,13 @@
+@echo off
+title SOUSA's FiveM Discord Bot
+color 5B
+echo apple rp server
+echo\
+echo                                                     Version 1.0
+echo\
+echo   Discord Server: https://discord.gg/AAAAAA
+echo\
+echo   Use "CTRL+C" to stop the bot
+echo\
+echo *******************************************************************************************
+node index.js
